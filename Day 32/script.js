b618500079fd48b4032d75d7a97c5692e044a165
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-11 14:57:49 +0530

    Add files via upload

---
+++ Day 32/script.js
@@ -0,0 +1,46 @@
+// console.log("Hera krishna");
+var a=5;
+var b=5.8;
+var c="xyz";
+// console.log(a,b,c)
+// console.log(typeof a,typeof b,typeof c)
+{   a+=1;//6
+    a-=2 
+    // console.log(a)
+}
+// console.log(a)  //in var the value become change.
+let p=3;
+let q=4.5;
+let r="sourin";
+
+{   let p=9;
+    p-=2;
+    // console.log(p);
+}
+// console.log(p);
+let m =4;
+let n=6.7;
+let i=null;
+const d= true;
+let e=undefined;
+let f="dsa";
+let g='amb';
+
+// console.log(m,n,i,d,e,f,g);
+// console.log(typeof m,typeof n,typeof i,typeof d,typeof e,typeof f,typeof g);
+
+let set={
+    "name":"sourin"
+}
+console.log(set);
+set.age=19
+console.log(set);
+set.salary="50 lacks per anm";
+console.log(set);
+// console.log(typeof set);
+
+
+
+
+
+
