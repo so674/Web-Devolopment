705320603bb62d857468cf799dfae215c4b1abac
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-11 15:38:42 +0530

    Add files via upload

---
+++ Day 33/script.js
@@ -0,0 +1,41 @@
+// console.log("hey");
+let a=9;
+let b=7;
+if(a<b){
+    // console.log("a is lessthan b");
+}
+else{
+    // console.log("a is graterthan b");
+}
+
+let p=6;
+let q=8;
+if(p<q){
+    // console.log("p is smallerthan q");
+}
+else if(p==q){
+    // console.log("p is equalto q");
+
+}
+else{
+    // console.log("p is lessthan q");
+
+}
+
+let x=67;
+let y=35;
+
+(x>y)?console.log("x is lessthan y") : console.log("x is grterthan y");
+
+// condition-->
+// if{(x>y)
+    // console.log("x is lessthan y") 
+    // else {console.log("x is grterthan y");
+    //  }
+// 
+let k=89;
+let l=90;
+// (k<l) ? console.log(k-l) : console.log(k+l)
+// (k>l) ? console.log(k-l) : console.log(k+l)
+(k<=l) ? console.log(k-l) : console.log(k+l)
+
